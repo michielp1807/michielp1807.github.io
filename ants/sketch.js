d1eee3d4922baa32678e0815f9c1826b6fabb502
Michiel <michiel.philipse@gmail.com>
2017-11-26 17:04:23 +0100

    Added mouse interactions & more customizability

--- ants/sketch.js
+++ ants/sketch.js
@@ -4,30 +4,86 @@ let pixelRows = 164;
 let antCount = 10;
 let ants = [];
 let pixels = [];
+let mouseAction = "";
 
 function setup() {
-   createCanvas(pixelCollumns*pixelSize, pixelRows*pixelSize);
+   let canvas = createCanvas(pixelCollumns*pixelSize, pixelRows*pixelSize);
+   canvas.parent("p5-sketch");
+   canvas.mousePressed(clicked);
    frameRate(60);
    colorMode(HSL,255);
-   
+
    for(let i=0; i<pixelCollumns; i++) {
 	   pixels[i] = [];
 	   for(let j=0; j<pixelCollumns; j++) {
 		   pixels[i][j] = false;
 	   }
    }
-   
+
    for (let i=0; i<antCount; i++) {
-	   ants.push(new ant(i));
+     let x = parseInt(random(pixelCollumns));
+     let y = parseInt(random(pixelRows));
+     let c = [parseInt((255/antCount)*i),200,120];
+     ants.push(new ant(x, y, c));
    }
-   
+
+   background(255);
 }
 
-function ant(i) {
-	this.x = parseInt(random(pixelCollumns));
-	this.y = parseInt(random(pixelRows));
+function restart() {
+  antCount = document.getElementById("antCount").value;
+  ants = [];
+
+  for(let i=0; i<pixelCollumns; i++) {
+    pixels[i] = [];
+    for(let j=0; j<pixelCollumns; j++) {
+      pixels[i][j] = false;
+    }
+  }
+
+  for (let i=0; i<antCount; i++) {
+    let x = parseInt(random(pixelCollumns));
+    let y = parseInt(random(pixelRows));
+    let c = [parseInt((255/antCount)*i),200,120];
+    ants.push(new ant(x, y, c));
+  }
+
+  background(255);
+}
+
+function clicked(m) {
+  let x = Math.floor(m.offsetX/pixelSize);
+  let y = Math.floor(m.offsetY/pixelSize);
+  //console.log("Clicked (x: "+x+",y: "+y+")");
+
+  switch (mouseAction) {
+    case "Add Ants":
+      let c = [parseInt(random(255)),200,120];
+      ants.push(new ant(x, y, c));
+      break;
+    case "Draw Blocks":
+      fill(0);
+      rect(x*pixelSize, y*pixelSize, pixelSize, pixelSize);
+      pixels[x][y] = true;
+      break;
+  }
+  return false;
+}
+
+function setMouseAction() {
+  mouseAction = document.getElementById("mouseAction").value;
+}
+
+function setFrameRateFromHTML() {
+  setFrameRate(parseInt(document.getElementById("frameRate").value));
+  console.log("setFrameRate");
+}
+
+function ant(_x, _y, _c) {
+	this.x = _x;
+	this.y = _y;
 	this.d = parseInt(random(4));
-	this.c = [parseInt((255/antCount)*i),200,120];
+	this.c = _c;
 	this.move = function() {
 		switch(this.d) { // move forward
 			case 0:
