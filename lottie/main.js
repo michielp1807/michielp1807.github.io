7c50bd67cad16f2672ca39a8b274880ef1a746f4
Michiel <michiel.philipse@gmail.com>
2020-03-15 15:39:20 +0100

    Added file loading and saving

--- lottie/main.js
+++ lottie/main.js
@@ -1,42 +1,113 @@
-let animationData = {"tgs":1,"v":"5.5.2","fr":30,"ip":0,"op":30,"w":512,"h":512,"nm":"rotate","ddd":0,"assets":[],"layers":[{"ddd":0,"ind":1,"ty":4,"nm":"Shape Layer 1","sr":1,"ks":{"r":{"a":1,"k":[{"i":{"x":[0.833],"y":[0.833]},"o":{"x":[0.167],"y":[0.167]},"t":0,"s":[0]},{"t":30,"s":[90]}]},"p":{"a":0,"k":[256,256,0]}},"ao":0,"shapes":[{"ty":"gr","it":[{"ty":"rc","d":3,"s":{"a":0,"k":[256,256]},"p":{"a":0,"k":[0,0]},"r":{"a":0,"k":0},"nm":"Rectangle Path 1","hd":false},{"ty":"fl","c":{"a":0,"k":[1,0,0,1]},"o":{"a":0,"k":100},"r":1,"bm":0,"nm":"Fill 1","hd":false},{"ty":"tr","p":{"a":0,"k":[0,-0.031]},"a":{"a":0,"k":[0,0]},"s":{"a":0,"k":[100,100]},"r":{"a":0,"k":0},"o":{"a":0,"k":100},"sk":{"a":0,"k":0},"sa":{"a":0,"k":0},"nm":"Transform"}],"nm":"Rectangle 1","bm":0,"hd":false}],"ip":0,"op":30,"st":0,"bm":0}]}
+const exampleAnimation = {"tgs":1,"v":"5.5.2","fr":30,"ip":0,"op":30,"w":512,"h":512,"nm":"rotate","ddd":0,"assets":[],"layers":[{"ddd":0,"ind":1,"ty":4,"nm":"Shape Layer 1","sr":1,"ks":{"r":{"a":1,"k":[{"i":{"x":[0.833],"y":[0.833]},"o":{"x":[0.167],"y":[0.167]},"t":0,"s":[0]},{"t":30,"s":[90]}]},"p":{"a":0,"k":[256,256,0]}},"ao":0,"shapes":[{"ty":"gr","it":[{"ty":"rc","d":3,"s":{"a":0,"k":[256,256]},"p":{"a":0,"k":[0,0]},"r":{"a":0,"k":0},"nm":"Rectangle Path 1","hd":false},{"ty":"fl","c":{"a":0,"k":[1,0,0,1]},"o":{"a":0,"k":100},"r":1,"bm":0,"nm":"Fill 1","hd":false},{"ty":"tr","p":{"a":0,"k":[0,-0.031]},"a":{"a":0,"k":[0,0]},"s":{"a":0,"k":[100,100]},"r":{"a":0,"k":0},"o":{"a":0,"k":100},"sk":{"a":0,"k":0},"sa":{"a":0,"k":0},"nm":"Transform"}],"nm":"Rectangle 1","bm":0,"hd":false}],"ip":0,"op":30,"st":0,"bm":0}]}
 
 
 let anim;
 let editor;
 const TAB_SIZE = 4;
 
-window.onload = function() {
+window.addEventListener("load", function() {
   const animationArea = document.getElementById("animationArea");
   const autoRefresh = document.getElementById("autoRefresh");
   const messageArea = document.getElementById("messageArea");
-
+  
+  const gzipFileInput = document.getElementById("gzipFileInput");
+  const jsonFileInput = document.getElementById("jsonFileInput");
+  
+  gzipFileInput.value = null;
+  jsonFileInput.value = null;
+  
+  // Setup editor
   editor = ace.edit("codeArea");
   editor.setTheme("ace/theme/xcode");
   editor.session.setMode("ace/mode/json");
-  editor.setValue(JSON.stringify(animationData, null, TAB_SIZE), 0);
   editor.session.setTabSize(TAB_SIZE);
-  editor.on("change", codeChange);
-  editor.gotoLine(0);
+  editor.on("change", function() {
+    if (autoRefresh.checked) {
+      setAnimation();
+    }
+  });
+  setCodeValue(exampleAnimation);
   setAnimation();
   
-  messageArea.onclick = function() {
+  // Setup message area
+  messageArea.addEventListener("click", function() {
     let err = messageArea.innerHTML;
     if (err.indexOf("SyntaxError: JSON.parse: ") == 0) {
       let lineCol = err.match(/\d+/g);
       let line = parseInt(lineCol[0]);
       let col = parseInt(lineCol[1]);
-      console.log(line);
       editor.navigateTo(line-1, col-1);
     }
     hideMessage();
-  }
+  });
+  
+  // Setup load Gzip button
+  gzipFileInput.addEventListener("change", function() {
+    try {
+      let reader = new FileReader();
+      reader.readAsBinaryString(gzipFileInput.files[0]);
+      reader.addEventListener("load", function(gzip) {
+        let jsonData = pako.ungzip(gzip.target.result);
+        let jsonString = new TextDecoder("utf-8").decode(jsonData);
+        setCodeValue(JSON.parse(jsonString));
+        jsonFileInput.value = null;
+      });
+    } catch (e) {
+      showMessage(e);
+      console.log(e)
+    }
+  });
+  
+  // Setup load JSON button
+  jsonFileInput.addEventListener("change", function() {
+    try {
+      let reader = new FileReader();
+      reader.readAsBinaryString(jsonFileInput.files[0]);
+      reader.addEventListener("load", function(json) {
+        setCodeValue(JSON.parse(json.target.result));
+        gzipFileInput.value = null;
+      });
+    } catch (e) {
+      showMessage(e);
+      console.log(e)
+    }
+  });
+  
+  // Setup save Gzip button
+  document.getElementById("saveAsGzip").addEventListener("click", function() {
+    let animationData = JSON.parse(editor.getValue());
+    let gzipData = pako.gzip(JSON.stringify(animationData));
+    //let gzipString = new TextDecoder("utf-8").decode(gzipData);
+    
+    downloadDataAsFile("sticker.tgs", gzipData);
+  });
+  
+  // Setup save JSON button
+  document.getElementById("saveAsJSON").addEventListener("click", function() {
+    let animationData = JSON.parse(editor.getValue());
+    downloadDataAsFile("lottie.json", JSON.stringify(animationData));
+  });
+});
+
+function downloadDataAsFile(filename, data) {
+  let blob = new Blob([data], {
+    type: "application/octet-stream"
+  });
+  let url = window.URL.createObjectURL(blob);
+
+  let element = document.createElement('a');
+  element.setAttribute("href", url);
+  element.setAttribute("download", filename);
+  element.style.display = 'none';
+  
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 }
 
-
-function codeChange() {
-  if (autoRefresh.checked) {
-    setAnimation();
-  }
+function setCodeValue(json) {
+  editor.setValue(JSON.stringify(json, null, TAB_SIZE), 0);
+  editor.gotoLine(0);
 }
 
 function formatCode() {
@@ -44,13 +115,13 @@ function formatCode() {
     editor.setValue(JSON.stringify(JSON.parse(editor.getValue()), null, TAB_SIZE), 0);
     hideMessage();
   } catch (e) {
-    handleError(e);
+    showMessage(e);
   }
 }
 
 function setAnimation() {
   try {
-    animationData = JSON.parse(editor.getValue());
+    let animationData = JSON.parse(editor.getValue());
     animationArea.innerHTML = "";
     let animData = {
       container: animationArea,
@@ -62,12 +133,8 @@ function setAnimation() {
     anim = bodymovin.loadAnimation(animData);
     hideMessage();
   } catch (e) {
-    handleError(e);
+    showMessage(e);
   }
-}
-
-function handleError(e) {
-  showMessage(e);
 }
 
 function showMessage(message) {
